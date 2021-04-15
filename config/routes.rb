443aa129e0265dee029e2f4870d144166c953351
Andrew Cordivari <andrew.cordivari@gmail.com>
2021-04-15 13:32:53 -0600

    getting markers seeded and rendering

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  resources :markers
   resources :stops
 
   get '/privacy', to: 'home#privacy'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Marker.create!([
+{ "address": "200 Mission Street, San Francisco, CA, USA", "latitude": "37.7919203","longitude": "-122.3955373"},
+{ "address": "Civic Center, San Francisco, CA, USA", "latitude": "37.7815533", "longitude": "-122.4156427"},
+{ "address": "SoMa West Skatepark, Central Freeway, San Francisco, CA, USA", "latitude": "37.7700959", "longitude": "-122.4214833"}
+])
